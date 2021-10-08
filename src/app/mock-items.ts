fb60b1d61ee658bc874c7ae821be6f1ccfe5c1bf
sen0ch <senoch@gmail.com>
2021-10-08 01:46:00 -0400

    Delete src directory

--- src/app/mock-items.ts
+++
@@ -1,8 +0,0 @@
-import { Item } from './item';
-
-export const ITEMS: Item[] = [
-  { id: 0, name: '0', headline: 'Headline 0', description: 'Description 0', image: 'anchor.jpg'  },
-  { id: 1, name: '1', headline: 'Headline 1', description: 'Description 1', image: 'item1.jpg'  },
-  { id: 2, name: '2', headline: 'Headline 2', description: 'Description 2', image: 'item2.jpg' },
-  { id: 3, name: '3', headline: 'Headline 3', description: 'Description 3', image: 'item3.jpg' },
-];
